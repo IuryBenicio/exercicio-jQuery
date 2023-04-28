fe11eeac4bc5838c815b0cb18f93b1623658f099
IuryBenicio <107493629+IuryBenicio@users.noreply.github.com>
2023-04-28 16:50:38 -0300

    Commit 1

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
+
+function verString(){
+    var stringTarefa = document.querySelector('p a')
+    console.log(stringTarefa)
+}
+
 $(document).ready(function(){
 
     $('.ListaDeTarefas').click(function(){
         $('#divAddTarefa').slideDown();
     })
+    var numeroTarefas = 0
+    var listaTarefas = []
 
     $('#divAddTarefa').on('submit',function(e){
         e.preventDefault();
+
+        numeroTarefas += 1
+
         var tarefaInput = $('.inputTarefa').val();
+
+        listaTarefas.push(tarefaInput)
+        console.log(listaTarefas)
+        console.log(numeroTarefas)
+
         var novaTarefa = $('<li style="text-decoration: none"></li>');
-        $(`<p><a class='tarefaFeita' href="#">${tarefaInput}</a></p>`).appendTo(novaTarefa);
+        $(`<p><a id='tarefa ${numeroTarefas}' href="#">- ${tarefaInput}</a></p>`).appendTo(novaTarefa);
         $(novaTarefa).appendTo('ul');
+
+        verString();
+
+
+        $('.inputTarefa').val('')
+
     })
 
-    $('li p').click(function(){
-        alert('TA FUNCIONANDO');
+    $('.lista').click(function(){
+        $('p a').css({"text-decoration" : "line-through"})
+        numeroTarefas -= 1
     })
 })
 
